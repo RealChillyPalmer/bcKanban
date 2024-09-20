14effb44f8755b31960a17e27eacb2997651cf61
chillypalmer <ivhighvibesxx@gmail.com>
2024-09-20 11:41:57 -0400

    Client folder complete

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,7 +2,27 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-  
+  try {
+
+  const response = await fetch('/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    } ,
+    body: JSON.stringify(userInfo)
+  });
+
+  if (!response.ok) {
+    const errData = await response.json();
+    throw new Error(`Error: ${errData.message}`);
+  }
+  const data = await response.json();
+
+  return data;
+} catch (err) {
+  console.log('User Error: ', err);
+  return Promise.reject('User Info Not Found');
+}
 }
 
 
--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,52 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const jwt = this.getToken();
+    return !!jwt && !this.isTokenExpired(jwt);
   }
   
-  isTokenExpired(token: string) {
+  isTokenExpired(jwt: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+
+    
+    const decode = jwtDecode<JwtPayload>(jwt);
+    console.log(decode);
+
+    if (decode?.exp && decode?.exp < Date.now()) {
+      return true;
+    }
+  } catch (error) {
+    return false;
   }
+}
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/')
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
